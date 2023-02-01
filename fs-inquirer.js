a70c12d23c3731c13b9f73e68e3d5426ec69901b
Colin Marshall <colin.marshall94@gmail.com>
2023-01-31 16:43:08 -0800

    added dependency for inquirer-autocomplete && fuzzy

---
+++ fs-inquirer.js
@@ -0,0 +1,6 @@
+const inquirer = require('inquirer')
+const fuzzy = require('fuzzy')
+const inquirerPrompt = require("inquirer-autocomplete-prompt");
+const path = require('path')
+const fs = require('fs')
+
